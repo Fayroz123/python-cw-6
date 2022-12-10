3c2f1086e2350174385a9c409dc7c152d68f5748
Fayroz <hndrfay45@gmail.com>
2022-12-10 20:11:25 +0300

    cw6done

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 # write your code here
+person = {
+'name': "fayroz",
+'age': "18",
+'hobbies': ["swimming", "drawing"],
+
+
+}
+print(person['name'])
+print(person['age'])
+person["country"] = "sirya"
+
+person['age']=19
+print(person['age'])
+print(person)
+print(len(person))
+person["hobbies"].append("reading") 
+print(person["hobbies"])
+
+
+def check_hobbies(person):
+    if len(person["hobbies"]) >= 3 :
+        print("WOW YOU ARE AMAZING")
+    
+    
+check_hobbies(person)
